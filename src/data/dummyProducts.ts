d327880135966478cd0bb4dd73af32bbc2a6f85b
Anshumesh Saini <anshumesh.saini@gmail.com>
2025-04-04 14:17:37 +0530

    abd

--- src/data/dummyProducts.ts
+++ src/data/dummyProducts.ts
@@ -430,17 +430,9 @@ const dummyProducts: Product[] = [
     category: "Decor",
     isOnSale: true,
     rating: 4
-<<<<<<< HEAD
+
   },
-  {
-    id: "47",
-    name: "Living Sculpture",
-    price: 599.99,
-    image: "https://images.unsplash.com/photo-1596001978150-82ce55cdbdcd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80",
-    category: "
-=======
-  }
+
 ];
 
 export default dummyProducts;
->>>>>>> ba9280b (🚀 Initial commit: Aether-Commerce project setup)
